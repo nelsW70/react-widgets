4c4c66cc6602a23700091046e8a2d3644b0fa28a
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-24 14:57:43 -0400

    add: useEffect in Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
   const [term, setTerm] = useState('');
 
+  useEffect(() => {
+    console.log('dfgfdgfd');
+  });
+
   return (
-    <div>
+    <div className="ui container">
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
