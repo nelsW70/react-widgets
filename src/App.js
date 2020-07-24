0d8ab845ab4d6b38c29037e62afb3e5784ba404c
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-24 14:42:47 -0400

    add: state to Search input

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+// import Accordion from './components/Accordion';
 import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end js framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite js library among engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
-  },
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end js framework',
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite js library among engineers',
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components',
+//   },
+// ];
 
 export default () => {
   return (
