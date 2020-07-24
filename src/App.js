45778ae5cf187036564d50159662663e66856500
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-24 10:10:54 -0400

    add: accordion component and hard-coded items array being passed down

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end js framework',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite js library among engineers',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components',
+  },
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
