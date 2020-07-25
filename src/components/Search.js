242cf475601d0767cdb9c72520abd4206b2f456b
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-25 09:37:10 -0400

    add: building out api request in useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,8 +4,20 @@ import axios from 'axios';
 const Search = () => {
   const [term, setTerm] = useState('');
 
-  useEffect(async () => {
-    await axios('fgfgfgf');
+  useEffect(() => {
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+
+    search();
   }, [term]);
 
   return (
