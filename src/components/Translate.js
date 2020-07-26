fbcc12be9e420f08bf96a87b589453f5d32e8747
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-26 09:47:00 -0400

    add: scaffold out Translate component

---
+++ src/components/Translate.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+
+const options = [
+  { lable: 'Afrikaans', value: 'af' },
+  { lable: 'Arabic', value: 'ar' },
+  { lable: 'Hinid', value: 'hi' },
+];
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+
+  return (
+    <div>
+      <Dropdown
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default Translate;
