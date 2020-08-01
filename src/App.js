1ee74f58d38a3bc6b19e19c9bedb3b03e0644057
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-01 10:16:06 -0400

    add: reusable router component implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
   {
@@ -25,37 +26,51 @@ const options = [
   { label: 'A Shade of Blue', value: 'blue' },
 ];
 
-const showAccordion = () => {
-  if (window.location.pathname === '/') {
-    return <Accordion items={items} />;
-  }
-};
+// const showAccordion = () => {
+//   if (window.location.pathname === '/') {
+//     return <Accordion items={items} />;
+//   }
+// };
 
-const showList = () => {
-  if (window.location.pathname === '/list') {
-    return <Search />;
-  }
-};
+// const showList = () => {
+//   if (window.location.pathname === '/list') {
+//     return <Search />;
+//   }
+// };
 
-const showDropdown = () => {
-  if (window.location.pathname === '/dropdown') {
-    return <Dropdown />;
-  }
-};
+// const showDropdown = () => {
+//   if (window.location.pathname === '/dropdown') {
+//     return <Dropdown />;
+//   }
+// };
 
-const showTranslate = () => {
-  if (window.location.pathname === '/translate') {
-    return <Translate />;
-  }
-};
+// const showTranslate = () => {
+//   if (window.location.pathname === '/translate') {
+//     return <Translate />;
+//   }
+// };
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div className="ui container">
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
